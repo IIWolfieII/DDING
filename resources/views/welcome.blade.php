4ab234c33c679596c96a31483be3e1cb238034b8
Sebby <s.klima@webdevel.cz>
2022-06-20 15:35:29 +0200

    gjallarhorns

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,20 +6,22 @@
                 <div class="flex flex-wrap flex-row items-center flex- sm:-mx-3 w-full">
                     <?php
 
+                    //GET Gjallahorn Request
                     $apiKey = 'a863ec719720455aafa2a1c2f9c1082b';
-
-                    $requestContent = [
-                        'headers' => [
-                            'Accept' => 'application/json',
-                            'Content-Type' => 'application/json',
-                            'X-API-Key' => $apiKey
-                        ]
-                    ];
-
                     try {
                         $client = new GuzzleHttp\Client();
 
-                        $apiRequest = $client->request('GET', 'https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/', $requestContent);
+                        $apiRequest = $client->request(
+                            'GET',
+                            'https://www.bungie.net/platform/Destiny2/Manifest/InventoryItem/1274330687/',
+                            [
+                                'headers' => [
+                                    'Accept' => 'application/json',
+                                    'Content-Type' => 'application/json',
+                                    'X-API-Key' => $apiKey
+                                ]
+                            ]
+                        );
 
                         $response = json_decode($apiRequest->getBody());
                         $data = $response->Response->data;
@@ -30,7 +32,8 @@
                     }
 
                     for ($i = 0; $i <= 30; $i++) { ?>
-                        <div class="rounded-sm h-16 w-16 m-1 cursor-pointer" style="background-image: url(https://www.bungie.net/{{ $item->icon }});"></div>
+                    <div class="rounded-sm h-16 w-16 m-1 cursor-pointer bg-center bg-no-repeat bg-cover"
+                         style="background-image: url(https://www.bungie.net/{{ $item->icon ?? ''}});"></div>
                     <?php } ?>
                 </div>
             </div>
